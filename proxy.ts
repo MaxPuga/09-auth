0181af6fb05705fc4c21e1bd0f50df57dc762996
Max <maxpuga87@gmail.com>
2026-05-18 09:48:51 +0200

    update all

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,4 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 import { checkSession } from '@/lib/api/serverApi';
 
@@ -7,10 +6,10 @@ const privateRoutes = ['/profile', '/notes'];
 const publicRoutes = ['/sign-in', '/sign-up'];
 
 export async function proxy(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
   const accessToken = request.cookies.get('accessToken')?.value;
   const refreshToken = request.cookies.get('refreshToken')?.value;
-
-  const pathname = request.nextUrl.pathname;
 
   const isPrivateRoute = privateRoutes.some(route =>
     pathname.startsWith(route)
@@ -20,7 +19,6 @@ export async function proxy(request: NextRequest) {
 
   let isAuthenticated = !!accessToken;
 
-  // якщо accessToken немає, але є refreshToken
   if (!accessToken && refreshToken) {
     try {
       const session = await checkSession();
@@ -30,13 +28,9 @@ export async function proxy(request: NextRequest) {
 
         const response = NextResponse.next();
 
-        if (session.data.accessToken) {
-          response.cookies.set('accessToken', session.data.accessToken);
-        }
+        response.cookies.set('accessToken', session.data.accessToken);
 
-        if (session.data.refreshToken) {
-          response.cookies.set('refreshToken', session.data.refreshToken);
-        }
+        response.cookies.set('refreshToken', session.data.refreshToken);
 
         return response;
       }
@@ -45,12 +39,10 @@ export async function proxy(request: NextRequest) {
     }
   }
 
-  // private routes
   if (isPrivateRoute && !isAuthenticated) {
     return NextResponse.redirect(new URL('/sign-in', request.url));
   }
 
-  // public routes
   if (isPublicRoute && isAuthenticated) {
     return NextResponse.redirect(new URL('/', request.url));
   }
