6990ef4f6b42a8963cf068656081a29f949bd9a5
Max <maxpuga87@gmail.com>
2026-05-18 09:38:30 +0200

    update all

--- app/(private routes)/profile/edit/page.tsx
+++ app/(private routes)/profile/edit/page.tsx
@@ -94,7 +94,7 @@ export default function EditProfilePage() {
             <button
               type="button"
               className={css.cancelButton}
-              onClick={() => router.push('/profile')}
+              onClick={() => router.back()}
             >
               Cancel
             </button>
--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,13 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+import { checkSession } from '@/lib/api/serverApi';
 
 const privateRoutes = ['/profile', '/notes'];
-
 const publicRoutes = ['/sign-in', '/sign-up'];
 
 export async function proxy(request: NextRequest) {
   const accessToken = request.cookies.get('accessToken')?.value;
-
   const refreshToken = request.cookies.get('refreshToken')?.value;
 
   const pathname = request.nextUrl.pathname;
@@ -17,14 +18,41 @@ export async function proxy(request: NextRequest) {
 
   const isPublicRoute = publicRoutes.some(route => pathname.startsWith(route));
 
-  const isAuthenticated = Boolean(accessToken || refreshToken);
+  let isAuthenticated = !!accessToken;
 
+  // якщо accessToken немає, але є refreshToken
+  if (!accessToken && refreshToken) {
+    try {
+      const session = await checkSession();
+
+      if (session.data.success) {
+        isAuthenticated = true;
+
+        const response = NextResponse.next();
+
+        if (session.data.accessToken) {
+          response.cookies.set('accessToken', session.data.accessToken);
+        }
+
+        if (session.data.refreshToken) {
+          response.cookies.set('refreshToken', session.data.refreshToken);
+        }
+
+        return response;
+      }
+    } catch {
+      isAuthenticated = false;
+    }
+  }
+
+  // private routes
   if (isPrivateRoute && !isAuthenticated) {
     return NextResponse.redirect(new URL('/sign-in', request.url));
   }
 
+  // public routes
   if (isPublicRoute && isAuthenticated) {
-    return NextResponse.redirect(new URL('/profile', request.url));
+    return NextResponse.redirect(new URL('/', request.url));
   }
 
   return NextResponse.next();
